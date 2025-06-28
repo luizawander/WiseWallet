a968344ef5aa20cd6bed41bb0f99d23e3be52041
Luiza Sales <154068580+luizawander@users.noreply.github.com>
2025-06-28 16:24:27 -0300

    adicionando config do swagger

--- expense-control/users/users_project/settings.py
+++ expense-control/users/users_project/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'accounts',
+    'drf_spectacular',
+    'drf_spectacular_sidecar',
+
 ]
 
 MIDDLEWARE = [
@@ -127,6 +130,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 AUTH_USER_MODEL = 'accounts.User'
